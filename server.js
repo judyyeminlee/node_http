43f5e09587a5cf79f4ac3593180c21940d8999bb
Judy Lee <judyyeminlee@gmail.com>
2024-07-16 10:34:14 -0400

    Finished Exercise

--- server.js
+++ server.js
@@ -6,25 +6,27 @@ const server = http.createServer((req, res) => {
   req.on("data", (chunk) => {
     chunks.push(chunk);
   });
-});
 
-req.on("end", () => {
-  const body = Buffer.concat(chunks).toString();
+  req.on("end", () => {
+    const body = Buffer.concat(chunks).toString();
 
-  if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("<h1>Welcome to the Home Page</h1>");
-  } else if (req.url === "/About") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.write(JSON.stringify({ name: "Judy", Info: "Animal Lover" }));
-  } else if (req.url === "/Echo") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.write(JSON.stringify({ method: req.method, url: req.url, body }));
-  } else {
-    
-  }
+    if (req.url === "/") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write("<h1>Welcome to the Home Page</h1>");
+    } else if (req.url === "/about") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify({ name: "Judy", Info: "Animal Lover" }));
+    } else if (req.url === "/echo") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify({ method: req.method, url: req.url, body }));
+    } else {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.write("<h1>404 Not Found</h1>");
+    }
+    res.end();
+  });
 });
 
 server.listen(5555, () => {
-  console.log("Can you see me?");
+  console.log("Listening");
 });
